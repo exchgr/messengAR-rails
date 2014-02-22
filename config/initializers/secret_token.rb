543ab6095ca5e4bf56fdca7d4761a4c8f5efed88
Dan Mundy <drm.mundy@gmail.com>
2014-02-22 17:09:43 -0500

    Initial commit: new Rails app and README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessengARRails::Application.config.secret_token = 'bb7aae4e49bd1f9639be643cc3926d80c03979e2b1886ddbd21dc77f0cac58bbe5a194141b6401c1fcbb9f610b0fdfde48b3d9edf80d8ff3e9c42ac6d227e91a'
