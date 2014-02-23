1a69a3e185da16daa585e1488288cd7bbedb1680
Dan Mundy <drm.mundy@gmail.com>
2014-02-23 12:48:53 -0500

    Adds recipient to stop 500 errors (BANDAID)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140223144855) do
+ActiveRecord::Schema.define(:version => 20140223174837) do
 
   create_table "messages", :force => true do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20140223144855) do
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.integer  "user_id"
+    t.string   "recipient"
   end
 
   create_table "users", :force => true do |t|
